dbf42068d63387c923332c60ed5d3ab67bbc6825
Melquisedeque <melki2011@gmail.com>
2020-07-27 07:41:18 -0300

    Adicionando pesquisa por cpf

--- src/pages/ListarCliente/index.js
+++ src/pages/ListarCliente/index.js
@@ -4,13 +4,14 @@ import api from './../../services/api';
 
 import './styles.css';
 import Menu from './../Menu';
+import MaskedInput from 'react-text-mask';
 
 
 export default function ListarCliente() {
     
     const [clientes, setClientes] = useState([]);
-    
-   
+    const [cpf, setCpf] = useState();
+
     useEffect(()=>{
         api.get('clientes')
         .then(response =>{
@@ -21,7 +22,16 @@ export default function ListarCliente() {
         })
     }, [])
     console.log(clientes);
-
+  
+    async function pesquisaPorCpf(){
+       await api.get(`clientes/cpf/${cpf}`)
+        .then(response =>{
+            setClientes(response.data.clientes)
+        })
+        .catch(e =>{
+            alert(`${e.response.data}`)
+        })
+    }
     return (
          <Container fluid >
             <div class="window">
@@ -34,6 +44,19 @@ export default function ListarCliente() {
                         { /* Formulario */}
                             <Row className='cadastro-container'>
                                 <div className="content">
+                                    <MaskedInput
+                                        id='cpf'
+                                        className='form-control'
+                                        required
+                                        mask={[/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/,]}
+                                        value={cpf}
+                                        onChange={e => setCpf(e.target.value)}
+                                        onKeyUp={(e) => {
+                                            setCpf(e.target.value);
+                                            console.log(cpf);
+                                            pesquisaPorCpf();
+                                        }}
+                                    />
                                     <Table striped bordered hover>
                                         <thead>
                                             <tr>
